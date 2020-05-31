f7c1b7c6360527655b5865a716606bc5d688388f
gabbo <gabbo@gabriela.pilegi>
2020-05-31 19:45:18 -0300

    break long words inside brag list

--- src/BragsScreen/Brags.jsx
+++ src/BragsScreen/Brags.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import { List, ListItem, ListItemIcon, Container } from '@material-ui/core'
 import { ChatBubbleRounded } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
 
-const Brags = ({ brags }) => (
-  <Container>
-    <List>
-      {brags.map((brag, index) => (
-        <ListItem key={index}>
-          <ListItemIcon>
-            <ChatBubbleRounded />
-          </ListItemIcon>
-          {brag}
-        </ListItem>
-      ))}
-    </List>
-  </Container>
-)
+const Brags = ({ brags }) => {
+  const useStyles = makeStyles((theme) => ({
+    brags: {
+      wordBreak: 'break-all',
+    },
+  }))
+
+  const classes = useStyles()
+
+  return (
+    <Container class={classes.brags}>
+      <List>
+        {brags.map((brag, index) => (
+          <ListItem key={index}>
+            <ListItemIcon>
+              <ChatBubbleRounded />
+            </ListItemIcon>
+            {brag}
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  )
+}
 
 export default Brags
