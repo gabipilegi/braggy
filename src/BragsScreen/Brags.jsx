6b71a3d82cb71707c4fbbea1b682a789b15b58bd
gabbo <gabbo@gabriela.pilegi>
2020-06-06 20:36:20 -0300

    Fix brag list layout

--- src/BragsScreen/Brags.jsx
+++ src/BragsScreen/Brags.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import { List, ListItem, ListItemIcon, Container } from '@material-ui/core'
 import { ChatBubbleRounded } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
 
-const Brags = ({ brags }) => (
-  <Container>
-    <List>
-      {brags.map((brag, index) => (
-        <ListItem key={index}>
-          <ListItemIcon>
-            <ChatBubbleRounded />
-          </ListItemIcon>
-          {brag}
-        </ListItem>
-      ))}
-    </List>
-  </Container>
-)
+const Brags = ({ brags }) => {
+  const useStyles = makeStyles((theme) => ({
+    brag: {
+      wordBreak: 'break-all',
+    },
+  }))
+
+  const classes = useStyles()
+
+  return (
+    <Container>
+      <List>
+        {brags.map((brag, index) => (
+          <div class={classes.brag}>
+            <ListItem key={index}>
+              <ListItemIcon>
+                <ChatBubbleRounded />
+              </ListItemIcon>
+              {brag}
+            </ListItem>
+          </div>
+        ))}
+      </List>
+    </Container>
+  )
+}
 
 export default Brags
