322c54f811a2eb7426ebda1cea07fd61f215578d
gabbo <gabbo@gabriela.pilegi>
2020-06-06 21:53:00 -0300

    use list item test

--- src/BragsScreen/Brags.jsx
+++ src/BragsScreen/Brags.jsx
@@ -1,29 +1,20 @@
 import React from 'react'
-import { List, ListItem, ListItemIcon, Container } from '@material-ui/core'
+import { List, ListItem, Container, ListItemText } from '@material-ui/core'
 import { ChatBubbleRounded } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
 
 const Brags = ({ brags }) => {
-  const useStyles = makeStyles((theme) => ({
-    brag: {
-      wordBreak: 'break-all',
-    },
-  }))
-
-  const classes = useStyles()
-
   return (
     <Container>
       <List>
         {brags.map((brag, index) => (
-          <div class={classes.brag}>
-            <ListItem key={index}>
-              <ListItemIcon>
-                <ChatBubbleRounded />
-              </ListItemIcon>
-              {brag}
-            </ListItem>
-          </div>
+          <ListItem key={index}>
+            <ListItemText
+              primary="20 Jun 2020"
+              secondary={brag}
+              style={{ wordBreak: 'break-all' }}
+            />
+          </ListItem>
         ))}
       </List>
     </Container>
